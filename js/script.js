aa235967300bd0f49e798ab679ee97afd2fa00b1
Sumaiya Aktar <sumaiyaaktar3029@gmail.com>
2023-06-07 20:55:00 +0600

    add

--- js/script.js
+++ js/script.js
@@ -142,3 +142,26 @@ document.getElementById("or").innerHTML =
   (x === 6 || y === 5) +
   "<br>" +
   (x === 6 || y === 3);
+// event
+const div1 = document.getElementById("myDIV");
+
+function myFunction() {
+  div1.style.animation = "mymove 4s 2";
+}
+
+div1.addEventListener("animationstart", myStartFunction);
+div1.addEventListener("animationiteration", myRepeatFunction);
+div1.addEventListener("animationend", myEndFunction);
+
+function myStartFunction() {
+  this.innerHTML = "The animation has started";
+  this.style.backgroundColor = "red";
+}
+function myRepeatFunction() {
+  this.innerHTML = "The animation was played again";
+  this.style.backgroundColor = "lightblue";
+}
+function myEndFunction() {
+  this.innerHTML = "The animation has completed";
+  this.style.backgroundColor = "lightgreen";
+}
